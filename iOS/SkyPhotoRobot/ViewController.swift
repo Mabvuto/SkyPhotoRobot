1432c06d439be964311192819d0cf8073a7197ae
Adam Eberbach <adameber@au1.ibm.com>
2015-04-08 16:37:48 +1000

    Initial commit - SkyPhotoRobot for https://2015.spaceappschallenge.org/challenge/my-sky-color/

---
+++ iOS/SkyPhotoRobot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkyPhotoRobot
+//
+//  Created by Adam Eberbach on 8/04/2015.
+//  Copyright (c) 2015 Gray Unicorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS/SkyPhotoRobotTests/SkyPhotoRobotTests.swift
@@ -0,0 +1,36 @@
+//
+//  SkyPhotoRobotTests.swift
+//  SkyPhotoRobotTests
+//
+//  Created by Adam Eberbach on 8/04/2015.
+//  Copyright (c) 2015 Gray Unicorn. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SkyPhotoRobotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
