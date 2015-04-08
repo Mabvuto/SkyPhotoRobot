149a3f312a323d49ad5fd769d70e95808228ce3e
Adam Eberbach <adameber@au1.ibm.com>
2015-04-08 17:30:23 +1000

    Add submodule RedBearLab/iOS, bridging header

---
+++ iOS/SkyPhotoRobotBridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  SkyPhotoRobotBridgingHeader.h
+//  SkyPhotoRobot
+//
+//  Created by Adam Eberbach on 8/04/2015.
+//  Copyright (c) 2015 Gray Unicorn. All rights reserved.
+//
+
+#ifndef SkyPhotoRobot_SkyPhotoRobotBridgingHeader_h
+#define SkyPhotoRobot_SkyPhotoRobotBridgingHeader_h
+
+#import "BLE.h"
+#import "BLEDefines.h"
+
+#endif
